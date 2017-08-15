309172574f894c558f4c8cbff9928d6854264348
jacksonleite <jlp@icomp.ufam.edu.br>
2017-08-15 16:51:06 -0400

    updates app design

--- app/src/main/java/br/com/tap4mobile/protochat/MainActivity.java
+++ app/src/main/java/br/com/tap4mobile/protochat/MainActivity.java
@@ -1,7 +1,6 @@
 package br.com.tap4mobile.protochat;
 
 import android.graphics.PorterDuff;
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -13,6 +12,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ImageView imgSendMessage;
+    ImageButton imgBtnConfirmar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imgSendMessage = (ImageView) findViewById(R.id.imgSendMessage);
+        imgBtnConfirmar = (ImageButton) findViewById(R.id.botao_confirmar);
 
         //Efeito para o botao enviar
         imgSendMessage.setOnTouchListener(new View.OnTouchListener() {
@@ -49,9 +50,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Efeito para o botao enviar
+        imgBtnConfirmar.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        ImageView view = (ImageView) v;
+                        //overlay is black with transparency of 0x77 (119)
+                        view.getDrawable().setColorFilter(0x77000000,PorterDuff.Mode.SRC_ATOP);
+                        view.invalidate();
+                        break;
+                    }
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL: {
+                        ImageView view = (ImageView) v;
+                        //clear the overlay
+                        view.getDrawable().clearColorFilter();
+                        view.invalidate();
+                        break;
+                    }
+                }
+
+                return false;
+            }
+        });
+
+
     }
 
     public void sendMessage(View view){
-        Toast.makeText(this, "Message Sent", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Descrição Adicionada", Toast.LENGTH_LONG).show();
+    }
+
+    public void recordOccurrence(View view){
+        Toast.makeText(this, "OCORRÊNCIA REGISTRADA COM SUCESSO", Toast.LENGTH_LONG).show();
     }
 }
